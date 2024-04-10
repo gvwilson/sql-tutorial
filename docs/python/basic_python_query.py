2ad3552eeaf4861250854afdff4a813bc77329eb
Greg Wilson <gvwilson@third-bit.com>
2024-04-10 11:14:21 -0400

    rebuild

--- docs/python/basic_python_query.py
+++ docs/python/basic_python_query.py
@@ -1,6 +1,8 @@
 import sqlite3
+import sys
 
-connection = sqlite3.connect("db/penguins.db")
+db_path = sys.argv[1]
+connection = sqlite3.connect(db_path)
 cursor = connection.execute("select count(*) from penguins;")
 rows = cursor.fetchall()
 print(rows)
--- docs/python/select_pandas.py
+++ docs/python/select_pandas.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import sqlite3
+import sys
 
-connection = sqlite3.connect("db/penguins.db")
+db_path = sys.argv[1]
+connection = sqlite3.connect(db_path)
 query = "select species, count(*) as num from penguins group by species;"
 df = pd.read_sql(query, connection)
 print(df)
